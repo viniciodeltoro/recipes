605b9981615c4610f52424130f769c982e41fab9
Vinicio Del Toro <viniciodeltoro@gmail.com>
2020-06-18 21:40:35 -0700

    added icon and cancel to search input

--- app/components/search-text-input.component.js
+++ app/components/search-text-input.component.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { TextInput, View, StyleSheet } from 'react-native';
+import { TextInput, View, StyleSheet, Text } from 'react-native';
 import {UiSizes} from '../helpers/ui-sizes';
 import {UiColors} from '../helpers/ui-colors';
+import FAIcon from 'react-native-vector-icons/FontAwesome5';
 
 export default class SearchTextInput extends React.Component{
   constructor(props){
@@ -30,11 +31,20 @@ export default class SearchTextInput extends React.Component{
   render(){
     return(
       <View style={styles.container}>
+        <View style={styles.iconContainer}>
+          <FAIcon name="search" size={16} color={UiColors.dark.inputText}/>
+        </View>
         <TextInput style={styles.searchInput}
           placeholderTextColor={UiColors.dark.inputText}
           onFocus={this.onTextInputFocus}
           onBlur={this.onTextInputBlur}
           placeholder="Search"/>
+        <View style={styles.cancelContainer}>
+          <View style={styles.inputRightPadding}></View>
+          <View style={{width: '100%', height: '100%', justifyContent: 'center'}}>
+            <Text style={styles.cancelText}>Cancel</Text>
+          </View>
+        </View>
       </View>
     )
   }
@@ -44,28 +54,55 @@ const styles = StyleSheet.create({
   container:{
     backgroundColor: 'transparent',
     height: UiSizes[Platform.OS].searchInputContainerHeight,
-    paddingVertical: UiSizes[Platform.OS].searchInputContainerPadding
+    width: '100%',
+    paddingVertical: UiSizes[Platform.OS].searchInputContainerPadding,
+    flexDirection: 'row',
+    alignItems: 'flex-start'
+  },
+  iconContainer: {
+    height: '100%',
+    width: 35,
+    backgroundColor: UiColors.dark.inputBackground,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomLeftRadius: 5,
+    borderTopLeftRadius: 5,
   },
   searchInput:{
     flex: 1, 
+    height: '100%',
     backgroundColor: UiColors.dark.inputBackground,
     color: UiColors.dark.hightEmphasis,
     borderWidth: 0,
     fontFamily: 'Nunito-Regular',
     fontSize: UiSizes[Platform.OS].searchInputFontSize,
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5,
-    borderBottomLeftRadius: 5,
-    borderBottomRightRadius: 5,
     ...Platform.select({
       ios: {
-        paddingLeft: 10,
         paddingRight: 10,
       },
       android: {
-        paddingLeft: 5,
         paddingRight: 5,
       }
     })    
   },
+  cancelContainer: {
+    height: '100%',
+    width: 100,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    backgroundColor: UiColors.dark.bars
+  },
+  inputRightPadding: {
+    height: '100%',
+    width: 30,
+    backgroundColor: UiColors.dark.inputBackground,
+    borderTopRightRadius: 5,
+    borderBottomRightRadius: 5,
+    marginRight: UiSizes[Platform.OS].searchInputContainerPadding
+  },
+  cancelText: {
+    fontFamily: 'Nunito-Regular',
+    fontSize: UiSizes[Platform.OS].searchInputFontSize,
+    color: UiColors.dark.primary
+  }
 });
